ac5e74e4ff30fbd0b71e4bcf360a5d600c1e252c
Rucha Pawar <ruchap2070@gmail.com>
2026-01-24 18:32:23 +0530

    Add files via upload

---
+++ program294.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class program294
+{
+    public static void main(String A[])
+        {
+            int Arr[] = {45,21,90,54,78};
+              
+            // this is for each loop
+            for(int no : Arr)
+            {
+                System.out.println(no);
+            }
+
+            int index = Arrays.binarySearch(Arr,90);
+            System.out.println("Element found at : "+index);
+
+        }
+}
